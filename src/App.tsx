db8d5211ebf975cd0f494760dcf18cc97dba7abf
mohammed sottou <mohammedsottou@gmail.com>
2025-12-19 14:29:28 +0100

    this is file for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { PrivateRoute } from './components/PrivateRoute'
+import { Signup } from './pages/auth/signup'
+import { Login } from './pages/auth/Login'
+import { ForgotPassword } from './pages/auth/forgot-password'
+import { ResetPassword } from './pages/auth/reset-password'
+import { Dashboard } from './pages/dashboard'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   )
 }
 
